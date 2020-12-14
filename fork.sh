add39b2755d57524a5e324b390ba3e70adefa4b1
James Turner <james.turner01@trinitymirror.com>
2020-12-14 12:46:43 +0000

    No intermediate file and removing fork.sh from the repo

--- fork.sh
+++ fork.sh
@@ -4,7 +4,6 @@ CURR_DIR=$(pwd)
 TMP_DIR=$(mktemp -d)
 git clone https://github.com/james-turner/github-to-ci-in-5-minutes.git $TMP_DIR
 cd $TMP_DIR
-git archive --format=tar HEAD -o archive.tar
-tar -xvf archive.tar -C $CURR_DIR
-cd $CURR_DIR
+git archive --format=tar HEAD | (cd $CURR_DIR && tar xf -)
+rm fork.sh
 
