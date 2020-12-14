7a2578ad6ed5a2538012a2dcbf3c579592ceb001
James Turner <james.turner01@trinitymirror.com>
2020-12-14 12:49:48 +0000

    Directory change is not inline

--- fork.sh
+++ fork.sh
@@ -5,5 +5,6 @@ TMP_DIR=$(mktemp -d)
 git clone https://github.com/james-turner/github-to-ci-in-5-minutes.git $TMP_DIR
 cd $TMP_DIR
 git archive --format=tar HEAD | (cd $CURR_DIR && tar xf -)
+cd $CURR_DIR
 rm fork.sh
 
